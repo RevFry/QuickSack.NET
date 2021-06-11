606d675b5feb749452af7cf2e11fb04c33db0b74
Andrew Weller <revfry@andrews-air.lan>
2021-06-11 00:22:15 -0400

    Adds analytics

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Net.Http;
+using System.Net.Http.Json;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Text;
@@ -10,6 +11,7 @@ using Microsoft.Extensions.Logging;
 using QuickSack.Shared;
 using QuickSack.Client.Code;
 using Blazored.LocalStorage;
+using Blazor.Analytics;
 
 namespace QuickSack.Client
 {
@@ -22,6 +24,13 @@ namespace QuickSack.Client
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddBlazoredLocalStorage();
+
+            using(HttpClient httpClient = new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) })
+            {
+                GTag gTag = await httpClient.GetFromJsonAsync<GTag>("gtag.json");
+                builder.Services.AddGoogleAnalytics(gTag.GoogleTag);
+            }
+            
             builder.Services.AddScoped<FeedRepository>();
             builder.Services.AddSingleton<SearchState>();
             builder.Services.AddSingleton<ToasterState>();
---
+++ Shared/GTag.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace QuickSack.Shared
+{
+    public class GTag
+    {
+        public string GoogleTag { get; set; }
+    }
+}
